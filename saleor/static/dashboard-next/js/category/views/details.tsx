c07d88a41c797d87ae3697f40e5a33ddbd7d1446
dominik-zeglen <flesz3@o2.pl>
2018-03-16 14:52:05 +0100

    Add fields and state to product filter widget

--- saleor/static/dashboard-next/js/category/views/details.tsx
+++ saleor/static/dashboard-next/js/category/views/details.tsx
@@ -1,35 +1,37 @@
+import * as React from "react";
+import { stringify as stringifyQs } from "qs";
+import Add from "material-ui-icons/Add";
+import ArrowBack from "material-ui-icons/ArrowBack";
 import Button from "material-ui/Button";
+import Card from "material-ui/Card";
 import Divider from "material-ui/Divider";
+import Drawer from "material-ui/Drawer";
+import FilterListIcon from "material-ui-icons/FilterList";
 import Grid from "material-ui/Grid";
 import IconButton from "material-ui/IconButton";
 import Toolbar from "material-ui/Toolbar";
 import Typography from "material-ui/Typography";
-import { withStyles, WithStyles } from "material-ui/styles";
-import ArrowBack from "material-ui-icons/ArrowBack";
-import Add from "material-ui-icons/Add";
-import * as React from "react";
-import { Link } from "react-router-dom";
 import { Component } from "react";
-import Card from "material-ui/Card";
-import FilterListIcon from "material-ui-icons/FilterList";
-import Drawer from "material-ui/Drawer";
+import { Link } from "react-router-dom";
+import { withStyles, WithStyles } from "material-ui/styles";
 
 import {
-  categoryPropertiesQuery,
   TypedCategoryPropertiesQuery,
-  rootCategoryChildrenQuery,
-  TypedRootCategoryChildrenQuery
+  TypedRootCategoryChildrenQuery,
+  categoryPropertiesQuery,
+  rootCategoryChildrenQuery
 } from "../queries";
-import CategoryProperties from "../components/CategoryProperties";
 import CategoryList from "../components/CategoryList";
-import ProductList from "../components/ProductList";
-import { categoryShowUrl, categoryAddUrl } from "../index";
+import CategoryProperties from "../components/CategoryProperties";
+import FilterCard from "../../components/cards/FilterCard";
+import Hidden from "material-ui/Hidden";
+import Navigator from "../../components/Navigator";
 import PageHeader from "../../components/PageHeader";
+import ProductFilters from "../components/ProductFilters";
+import ProductList from "../components/ProductList";
 import Skeleton from "../../components/Skeleton";
 import i18n from "../../i18n";
-import Hidden from "material-ui/Hidden";
-import FilterCard from "../../components/cards/FilterCard";
-import { ProductFilters } from "../components/ProductFilters";
+import { categoryShowUrl, categoryAddUrl } from "../index";
 
 const decorate = withStyles(theme => ({
   title: {
@@ -50,6 +52,14 @@ interface CategoryDetailsState {
   isFilterMenuOpened: boolean;
 }
 
+// TODO: Replace when API is ready
+const dummyProductTypes = [
+  { id: "123123123", name: "Type 1" },
+  { id: "123123124", name: "Type 2" },
+  { id: "123123125", name: "Type 3" },
+  { id: "123123126", name: "Type 4" }
+];
+
 const CategoryDetails = decorate(
   class CategoryDetailsComponent extends Component<
     CategoryDetailsProps & WithStyles<"title" | "subtitle">,
@@ -67,151 +77,170 @@ const CategoryDetails = decorate(
       const { classes, filters, id } = this.props;
       if (id) {
         return (
-          <TypedCategoryPropertiesQuery
-            query={categoryPropertiesQuery}
-            variables={{ id, first: 12 }}
-            fetchPolicy="network-only"
-          >
-            {({ loading, error, data: { category }, fetchMore }) => {
-              if (error) {
-                return <span>not ok</span>;
-              }
-              const handleLoadMore = () => {
-                if (loading) {
-                  return;
-                }
-                return fetchMore({
-                  variables: {
-                    after: category.products.pageInfo.endCursor
-                  },
-                  updateQuery: (previousResult, { fetchMoreResult }) => {
-                    return {
-                      ...previousResult,
-                      category: {
-                        ...previousResult.category,
-                        products: {
-                          ...previousResult.category.products,
-                          edges: [
-                            ...previousResult.category.products.edges,
-                            ...fetchMoreResult.category.products.edges
-                          ],
-                          pageInfo: {
-                            ...fetchMoreResult.category.products.pageInfo
-                          }
-                        }
-                      }
-                    };
-                  }
-                });
-              };
+          <Navigator>
+            {navigate => {
+              const applyFilters = formState => () =>
+                navigate(`?${stringifyQs(formState)}`, true);
+              const clearFilters = () => navigate("?");
               return (
-                <Grid container spacing={24}>
-                  <Grid item xs={12}>
-                    <Grid container spacing={24}>
-                      <Grid item xs={12} md={9}>
-                        <CategoryProperties
-                          category={category}
-                          loading={loading}
-                        />
-                      </Grid>
-                    </Grid>
-                  </Grid>
-                  <Grid item xs={12}>
-                    <Grid container spacing={24}>
-                      <Grid item xs={12} md={9}>
-                        <Card>
-                          <PageHeader
-                            title={i18n.t("Subcategories", {
-                              context: "title"
-                            })}
-                          >
-                            <IconButton
-                              component={props => (
-                                <Link
-                                  to={
-                                    category ? categoryAddUrl(category.id) : "#"
+                <TypedCategoryPropertiesQuery
+                  query={categoryPropertiesQuery}
+                  variables={{ id, first: 12 }}
+                  fetchPolicy="network-only"
+                >
+                  {({ loading, error, data: { category }, fetchMore }) => {
+                    if (error) {
+                      return <span>not ok</span>;
+                    }
+                    const handleLoadMore = () => {
+                      if (loading) {
+                        return;
+                      }
+                      return fetchMore({
+                        variables: {
+                          after: category.products.pageInfo.endCursor
+                        },
+                        updateQuery: (previousResult, { fetchMoreResult }) => {
+                          return {
+                            ...previousResult,
+                            category: {
+                              ...previousResult.category,
+                              products: {
+                                ...previousResult.category.products,
+                                edges: [
+                                  ...previousResult.category.products.edges,
+                                  ...fetchMoreResult.category.products.edges
+                                ],
+                                pageInfo: {
+                                  ...fetchMoreResult.category.products.pageInfo
+                                }
+                              }
+                            }
+                          };
+                        }
+                      });
+                    };
+                    return (
+                      <Grid container spacing={24}>
+                        <Grid item xs={12}>
+                          <Grid container spacing={24}>
+                            <Grid item xs={12} md={9}>
+                              <CategoryProperties
+                                category={category}
+                                loading={loading}
+                              />
+                            </Grid>
+                          </Grid>
+                        </Grid>
+                        <Grid item xs={12}>
+                          <Grid container spacing={24}>
+                            <Grid item xs={12} md={9}>
+                              <Card>
+                                <PageHeader
+                                  title={i18n.t("Subcategories", {
+                                    context: "title"
+                                  })}
+                                >
+                                  <IconButton
+                                    component={props => (
+                                      <Link
+                                        to={
+                                          category
+                                            ? categoryAddUrl(category.id)
+                                            : "#"
+                                        }
+                                        {...props}
+                                      />
+                                    )}
+                                    disabled={loading}
+                                  >
+                                    <Add />
+                                  </IconButton>
+                                </PageHeader>
+                                <CategoryList
+                                  categories={
+                                    category &&
+                                    category.children &&
+                                    category.children.edges
                                   }
-                                  {...props}
                                 />
-                              )}
-                              disabled={loading}
-                            >
-                              <Add />
-                            </IconButton>
-                          </PageHeader>
-                          <CategoryList
-                            categories={
-                              category &&
-                              category.children &&
-                              category.children.edges
-                            }
-                          />
-                        </Card>
-                      </Grid>
-                    </Grid>
-                  </Grid>
-                  <Grid item xs={12}>
-                    <Grid container spacing={24}>
-                      <Grid item xs={12} md={9}>
-                        <Card>
-                          <PageHeader
-                            title={i18n.t("Products", { context: "title" })}
-                          >
-                            <IconButton
-                              component={props => <Link to="#" {...props} />}
-                              disabled={loading}
-                            >
-                              <Add />
-                            </IconButton>
-                            <Hidden mdUp implementation="css">
-                              <IconButton
-                                disabled={loading}
-                                onClick={this.handleFilterMenuOpen}
+                              </Card>
+                            </Grid>
+                          </Grid>
+                        </Grid>
+                        <Grid item xs={12}>
+                          <Grid container spacing={24}>
+                            <Grid item xs={12} md={9}>
+                              <Card>
+                                <PageHeader
+                                  title={i18n.t("Products", {
+                                    context: "title"
+                                  })}
+                                >
+                                  <IconButton
+                                    component={props => (
+                                      <Link to="#" {...props} />
+                                    )}
+                                    disabled={loading}
+                                  >
+                                    <Add />
+                                  </IconButton>
+                                  <Hidden mdUp implementation="css">
+                                    <IconButton
+                                      disabled={loading}
+                                      onClick={this.handleFilterMenuOpen}
+                                    >
+                                      <FilterListIcon />
+                                    </IconButton>
+                                  </Hidden>
+                                </PageHeader>
+                                <ProductList
+                                  products={
+                                    category &&
+                                    category.products &&
+                                    category.products.edges
+                                  }
+                                  handleLoadMore={handleLoadMore}
+                                  canLoadMore={
+                                    category &&
+                                    category.products &&
+                                    category.products.pageInfo &&
+                                    category.products.pageInfo.hasNextPage
+                                  }
+                                />
+                              </Card>
+                            </Grid>
+                            <Grid item xs={12} md={3}>
+                              <Hidden smDown implementation="css">
+                                <ProductFilters
+                                  handleSubmit={applyFilters}
+                                  handleClear={clearFilters}
+                                  productTypes={dummyProductTypes}
+                                  formState={filters}
+                                />
+                              </Hidden>
+                              <Drawer
+                                open={this.state.isFilterMenuOpened}
+                                onClose={this.handleFilterMenuOpen}
+                                anchor="bottom"
                               >
-                                <FilterListIcon />
-                              </IconButton>
-                            </Hidden>
-                          </PageHeader>
-                          <ProductList
-                            products={
-                              category &&
-                              category.products &&
-                              category.products.edges
-                            }
-                            handleLoadMore={handleLoadMore}
-                            canLoadMore={
-                              category &&
-                              category.products &&
-                              category.products.pageInfo &&
-                              category.products.pageInfo.hasNextPage
-                            }
-                          />
-                        </Card>
+                                <ProductFilters
+                                  handleSubmit={applyFilters}
+                                  handleClear={clearFilters}
+                                  productTypes={dummyProductTypes}
+                                  formState={filters}
+                                />
+                              </Drawer>
+                            </Grid>
+                          </Grid>
+                        </Grid>
                       </Grid>
-                      <Grid item xs={12} md={3}>
-                        <Hidden smDown implementation="css">
-                          <ProductFilters
-                            handleClear={() => {}}
-                            handleSubmit={() => {}}
-                          />
-                        </Hidden>
-                        <Drawer
-                          open={this.state.isFilterMenuOpened}
-                          onClose={this.handleFilterMenuOpen}
-                          anchor="bottom"
-                        >
-                          <ProductFilters
-                            handleClear={() => {}}
-                            handleSubmit={() => {}}
-                          />
-                        </Drawer>
-                      </Grid>
-                    </Grid>
-                  </Grid>
-                </Grid>
+                    );
+                  }}
+                </TypedCategoryPropertiesQuery>
               );
             }}
-          </TypedCategoryPropertiesQuery>
+          </Navigator>
         );
       }
       return (
